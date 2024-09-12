8304f698194fd5ee091eeacd5a7e67f3da58ca9e
Selen Gora <goraselen@gmail.com>
2024-09-12 17:46:42 +0200

    removed unnecessary comments

--- form-wave-animation/script.js
+++ form-wave-animation/script.js
@@ -1,12 +1,3 @@
-/* 
-1- grab labels
-2- split labels' innerText with foreach array and split function
-3- manipulate with map function and make transition with ``
-4- merge splitted letters again with join fnc
-
-*/
-
-
 
 const labels = document.querySelectorAll('.form-container label');
 
